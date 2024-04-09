a261b5da4ea66488242ae9e7211b25655c64f650
--global <ravindran.shan@gmail.com>
2024-04-09 23:25:05 +0530

    new programs

--- DynamicProg/countSteps.py
+++ DynamicProg/countSteps.py
@@ -13,8 +13,14 @@ def climbStairs(n):
     print('hello')
 
     for i in range(3, 6):
-        # table[i] = table[i-1] + table[i-2]
+
+
         y = table[(i) % 3] = table[(i + 1) % 3] + table[(i + 2) % 3]
+        # above is a memory efficient method  using only 3 vars
+        # otherwise you need to create table[n] ints
+        # but, here it uses only 3 vars irrespective of n
+        # table[i] = table[i-1] + table[i-2]
+
         print(y)
     print(f"i is {0}",i)
     return table[(i) % 3]
